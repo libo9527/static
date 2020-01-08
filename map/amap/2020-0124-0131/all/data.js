66cac31cc2cd6356274da4b66b9c2d13e66a7790
bli <bli@skystartrade.com>
2020-01-08 11:47:12 +0800

    update

--- map/amap/2020-0124-0131/all/data.js
+++ map/amap/2020-0124-0131/all/data.js
@@ -28,16 +28,6 @@ const administrativeCenter = [{
     infoBody: "",
     type: "行政区",
     visible: true
-}, {
-    id: 2,
-    title: "长沙",
-    position: [112.93886,28.22778],
-    awesomeIcon: "star",
-    iconLabel: {style: {color: "salmon", fontSize: "14px"}},
-    iconStyle: "black",
-    infoBody: "",
-    type: "行政区",
-    visible: true
 }];
 const traffic = [{
     id: 6,
@@ -79,13 +69,203 @@ const traffic = [{
     infoBody: "",
     type: "火车站",
     visible: true
+}, {
+    id: 6,
+    title: "大荔站",
+    position: [109.92573, 34.83477],
+    awesomeIcon: "subway",
+    iconLabel: {style: {color: "#333", fontSize: "14px"}},
+    iconStyle: "orange",
+    infoBody: "",
+    type: "动车/高铁站",
+    visible: true
+}, {
+    id: 6,
+    title: "太原南站",
+    position: [112.610668, 37.791537],
+    awesomeIcon: "subway",
+    iconLabel: {style: {color: "#333", fontSize: "14px"}},
+    iconStyle: "orange",
+    infoBody: "",
+    type: "动车/高铁站",
+    visible: true
+}, {
+    id: 6,
+    title: "北京西站",
+    position: [116.321592, 39.894806],
+    awesomeIcon: "subway",
+    iconLabel: {style: {color: "#333", fontSize: "14px"}},
+    iconStyle: "orange",
+    infoBody: "",
+    type: "动车/高铁站",
+    visible: true
 }];
 const hotel = [];
 const food = [];
 const scenery = [{
     id: 14,
     title: "故宫",
-    position: [116.397027,39.918056],
+    position: [116.397027, 39.918056],
+    awesomeIcon: "photo",
+    iconLabel: {style: {color: "lightgreen", fontSize: "14px"}},
+    iconStyle: "green",
+    infoBody: "",
+    type: "景区",
+    visible: true
+}, {
+    id: 14,
+    title: "八达岭长城",
+    position: [116.01548, 40.356585],
+    awesomeIcon: "photo",
+    iconLabel: {style: {color: "lightgreen", fontSize: "14px"}},
+    iconStyle: "green",
+    infoBody: "",
+    type: "景区",
+    visible: true
+}, {
+    id: 14,
+    title: "恭王府",
+    position: [116.386281, 39.937162],
+    awesomeIcon: "photo",
+    iconLabel: {style: {color: "lightgreen", fontSize: "14px"}},
+    iconStyle: "green",
+    infoBody: "",
+    type: "景区",
+    visible: true
+}, {
+    id: 14,
+    title: "鸟巢",
+    position: [116.396514, 39.992864],
+    awesomeIcon: "photo",
+    iconLabel: {style: {color: "lightgreen", fontSize: "14px"}},
+    iconStyle: "green",
+    infoBody: "",
+    type: "景区",
+    visible: true
+}, {
+    id: 14,
+    title: "什刹海",
+    position: [116.384467, 39.942209],
+    awesomeIcon: "photo",
+    iconLabel: {style: {color: "lightgreen", fontSize: "14px"}},
+    iconStyle: "green",
+    infoBody: "",
+    type: "景区",
+    visible: true
+}, {
+    id: 14,
+    title: "前门大街",
+    position: [116.39805973, 39.89859654],
+    awesomeIcon: "photo",
+    iconLabel: {style: {color: "lightgreen", fontSize: "14px"}},
+    iconStyle: "green",
+    infoBody: "",
+    type: "景区",
+    visible: true
+}, {
+    id: 14,
+    title: "水立方",
+    position: [116.390328, 39.99263],
+    awesomeIcon: "photo",
+    iconLabel: {style: {color: "lightgreen", fontSize: "14px"}},
+    iconStyle: "green",
+    infoBody: "",
+    type: "景区",
+    visible: true
+}, {
+    id: 14,
+    title: "天坛公园",
+    position: [116.41049, 39.88119],
+    awesomeIcon: "photo",
+    iconLabel: {style: {color: "lightgreen", fontSize: "14px"}},
+    iconStyle: "green",
+    infoBody: "",
+    type: "景区",
+    visible: true
+}, {
+    id: 14,
+    title: "首都博物馆",
+    position: [116.341985, 39.906427],
+    awesomeIcon: "photo",
+    iconLabel: {style: {color: "lightgreen", fontSize: "14px"}},
+    iconStyle: "green",
+    infoBody: "",
+    type: "景区",
+    visible: true
+}, {
+    id: 14,
+    title: "十三陵",
+    position: [116.225269, 40.258082],
+    awesomeIcon: "photo",
+    iconLabel: {style: {color: "lightgreen", fontSize: "14px"}},
+    iconStyle: "green",
+    infoBody: "",
+    type: "景区",
+    visible: true
+}, {
+    id: 14,
+    title: "雍和宫",
+    position: [116.41726, 39.94759],
+    awesomeIcon: "photo",
+    iconLabel: {style: {color: "lightgreen", fontSize: "14px"}},
+    iconStyle: "green",
+    infoBody: "",
+    type: "景区",
+    visible: true
+}, {
+    id: 14,
+    title: "圆明园",
+    position: [116.303468, 40.006088],
+    awesomeIcon: "photo",
+    iconLabel: {style: {color: "lightgreen", fontSize: "14px"}},
+    iconStyle: "green",
+    infoBody: "",
+    type: "景区",
+    visible: true
+}, {
+    id: 14,
+    title: "慕田峪长城",
+    position: [116.564567, 40.428447],
+    awesomeIcon: "photo",
+    iconLabel: {style: {color: "lightgreen", fontSize: "14px"}},
+    iconStyle: "green",
+    infoBody: "",
+    type: "景区",
+    visible: true
+}, {
+    id: 14,
+    title: "南锣鼓巷",
+    position: [116.40319347, 39.9372012],
+    awesomeIcon: "photo",
+    iconLabel: {style: {color: "lightgreen", fontSize: "14px"}},
+    iconStyle: "green",
+    infoBody: "",
+    type: "景区",
+    visible: true
+}, {
+    id: 14,
+    title: "颐和园",
+    position: [116.272362, 39.99299],
+    awesomeIcon: "photo",
+    iconLabel: {style: {color: "lightgreen", fontSize: "14px"}},
+    iconStyle: "green",
+    infoBody: "",
+    type: "景区",
+    visible: true
+}, {
+    id: 14,
+    title: "北海公园",
+    position: [116.388715, 39.927577],
+    awesomeIcon: "photo",
+    iconLabel: {style: {color: "lightgreen", fontSize: "14px"}},
+    iconStyle: "green",
+    infoBody: "",
+    type: "景区",
+    visible: true
+}, {
+    id: 14,
+    title: "天安门广场",
+    position: [116.397827, 39.90374],
     awesomeIcon: "photo",
     iconLabel: {style: {color: "lightgreen", fontSize: "14px"}},
     iconStyle: "green",
---
+++ map/amap/utils/links.md
@@ -0,0 +1,11 @@
+## 坐标拾取器
+
+[经纬度查询手机版—GPSspg](http://www.gpsspg.com/maps.htm)
+
+## 图标
+
+[Font Awesome](http://fontawesome.dashgame.com/)
+
+## 查询列车时刻表
+
+[火车票网](http://www.huochepiao.com/search)

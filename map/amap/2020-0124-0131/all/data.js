c2584e0443a297c34f959b4a119248adcbeeab63
bli <bli@skystartrade.com>
2020-01-10 15:34:34 +0800

    update

--- map/amap/2020-0124-0131/all/data.js
+++ map/amap/2020-0124-0131/all/data.js
@@ -101,7 +101,47 @@ const traffic = [{
     visible: true
 }];
 const hotel = [];
-const food = [];
+const food = [{
+    id: 74,
+    title: "全聚德前门店",
+    position: [116.39823139, 39.89649761],
+    awesomeIcon: "cutlery",
+    iconLabel: {style: {color: "gray", fontSize: "14px"}},
+    iconStyle: "lightpink",
+    infoBody: "<div>最著名的老字号烤鸭店，迄今已有140多年的历史，以经营传统挂炉烤鸭闻名海内外。</div><div style='text-align: right'></div>",
+    type: "美食",
+    visible: true
+}, {
+    id: 74,
+    title: "王府井小吃街",
+    position: [116.412431, 39.913387],
+    awesomeIcon: "cutlery",
+    iconLabel: {style: {color: "gray", fontSize: "14px"}},
+    iconStyle: "lightpink",
+    infoBody: "<div>北京及各地风味小吃和旅游纪念品，价格略贵。</div><div style='text-align: right'></div>",
+    type: "美食",
+    visible: true
+}, {
+    id: 74,
+    title: "姚记炒肝店",
+    position: [116.396757, 39.94093],
+    awesomeIcon: "cutlery",
+    iconLabel: {style: {color: "gray", fontSize: "14px"}},
+    iconStyle: "lightpink",
+    infoBody: "<div>北京及各地风味小吃和旅游纪念品，价格略贵。</div><div style='text-align: right'></div>",
+    type: "美食",
+    visible: true
+}, {
+    id: 74,
+    title: "东来顺王府井饭庄",
+    position: [116.411525, 39.911128],
+    awesomeIcon: "cutlery",
+    iconLabel: {style: {color: "gray", fontSize: "14px"}},
+    iconStyle: "lightpink",
+    infoBody: "<div>北京及各地风味小吃和旅游纪念品，价格略贵。</div><div style='text-align: right'></div>",
+    type: "美食",
+    visible: true
+}];
 const scenery = [{
     id: 14,
     title: "故宫",
@@ -116,6 +156,16 @@ const scenery = [{
     id: 14,
     title: "八达岭长城",
     position: [116.01548, 40.356585],
+    awesomeIcon: "photo",
+    iconLabel: {style: {color: "lightgreen", fontSize: "14px"}},
+    iconStyle: "green",
+    infoBody: "",
+    type: "景区",
+    visible: true
+}, {
+    id: 14,
+    title: "798艺术区",
+    position: [116.49462, 39.985028],
     awesomeIcon: "photo",
     iconLabel: {style: {color: "lightgreen", fontSize: "14px"}},
     iconStyle: "green",
@@ -273,7 +323,87 @@ const scenery = [{
     type: "景区",
     visible: true
 }];
-const innerScenery = [];
+const innerScenery = [{
+    id: 73,
+    title: "人民大会堂",
+    position: [116.39382, 39.90517],
+    awesomeIcon: "arrows",
+    iconLabel: {style: {color: "green", fontSize: "14px"}},
+    iconStyle: "lightgreen",
+    infoBody: "",
+    type: "内部景点",
+    visible: true
+}, {
+    id: 73,
+    title: "中国国家博物馆",
+    position: [116.401397, 39.905408],
+    awesomeIcon: "arrows",
+    iconLabel: {style: {color: "green", fontSize: "14px"}},
+    iconStyle: "lightgreen",
+    infoBody: "",
+    type: "内部景点",
+    visible: true
+}, {
+    id: 73,
+    title: "景山公园",
+    position: [116.396776, 39.925847],
+    awesomeIcon: "arrows",
+    iconLabel: {style: {color: "green", fontSize: "14px"}},
+    iconStyle: "lightgreen",
+    infoBody: "",
+    type: "内部景点",
+    visible: true
+}, {
+    id: 73,
+    title: "劳动人民文化宫（太庙）",
+    position: [116.399864, 39.911617],
+    awesomeIcon: "arrows",
+    iconLabel: {style: {color: "green", fontSize: "14px"}},
+    iconStyle: "lightgreen",
+    infoBody: "",
+    type: "内部景点",
+    visible: true
+}, {
+    id: 73,
+    title: "天安门城楼",
+    position: [116.397585, 39.908738],
+    awesomeIcon: "arrows",
+    iconLabel: {style: {color: "green", fontSize: "14px"}},
+    iconStyle: "lightgreen",
+    infoBody: "",
+    type: "内部景点",
+    visible: true
+}, {
+    id: 73,
+    title: "中山公园",
+    position: [116.394416, 39.910685],
+    awesomeIcon: "arrows",
+    iconLabel: {style: {color: "green", fontSize: "14px"}},
+    iconStyle: "lightgreen",
+    infoBody: "",
+    type: "内部景点",
+    visible: true
+}, {
+    id: 73,
+    title: "正阳门俗称前门",
+    position: [116.397908, 39.899365],
+    awesomeIcon: "arrows",
+    iconLabel: {style: {color: "green", fontSize: "14px"}},
+    iconStyle: "lightgreen",
+    infoBody: "",
+    type: "内部景点",
+    visible: true
+}, {
+    id: 73,
+    title: "毛主席纪念堂",
+    position: [116.397765, 39.902547],
+    awesomeIcon: "arrows",
+    iconLabel: {style: {color: "green", fontSize: "14px"}},
+    iconStyle: "lightgreen",
+    infoBody: "",
+    type: "内部景点",
+    visible: true
+}];
 
 let data = [];
 data.push(...administrativeCenter);

a045e5baa75354f6d84081f380ffc8c97ccdcbd8
bli <bli@skystartrade.com>
2020-01-15 17:26:04 +0800

    update

---
+++ map/amap/2020-0117-0120/hengshan/timeline/data.js
@@ -0,0 +1,133 @@
+const data = [{
+    timestamp: '2020-01-18 01:05:00',
+    imgUrls: [],
+    title: 'K6618 深圳站 - 株洲站',
+    content: '从深圳站出发，前往株洲'
+}, {
+    timestamp: '2020-01-18 10:43:00',
+    imgUrls: [],
+    title: 'K6618 深圳站 - 株洲站',
+    content: '到达株洲站'
+}, {
+    timestamp: '2020-01-18 10:59:00',
+    imgUrls: [],
+    title: 'K9001 株洲站 - 衡山站',
+    content: '从株洲站出发，前往衡山'
+}, {
+    timestamp: '2020-01-18 11:49:00',
+    imgUrls: [],
+    title: 'K9001 株洲站 - 衡山站',
+    content: '到达衡山站'
+}, {
+    timestamp: '2020-01-18 12:00:00',
+    imgUrls: [
+        'https://06imgmini.eastday.com/mobile/20191130/20191130193812_c6baa20e2c69471efba5a16dfbb58f8b_1.jpeg'
+    ],
+    title: '城际巴士 衡山站 - 南岳衡山风景区',
+    content: '衡山火车站——南岳衡山风景区<br>' +
+        '列车到达衡山火车站，在出站后约100米处可转乘汽车：<br>' +
+        '1）、乘坐“衡山火车站-南岳”的直达巴士：<br>' +
+        '票价（5元/人）,运营时间（早07:30---晚17:30）。<br>' +
+        '2）、乘坐“1路”公交车（1元/人）至“两路口汽车站”，<br>' +
+        '再转乘“衡山-南岳”的城际巴士（5元/人）到达南岳长途汽车站。<br>' +
+        '提示：公交车运营时间：（早06:30---晚18:30），<br>' +
+        '城际巴士运营时间：（早07:30---晚17:30）。<br>' +
+        '3）、如果您是晚间达到衡山，建议您乘坐出租车前往南岳衡山风景区：<br>' +
+        '票价约为（15元/人或者60/车）价格是按计表打价，如是淡季也可和司机讲价。'
+}, {
+    timestamp: '2020-01-18 13:00:00',
+    imgUrls: [],
+    title: '城际巴士 衡山站 - 南岳衡山风景区',
+    content: '到达南岳衡山风景区'
+},
+
+
+    {
+        timestamp: '2020-01-19 09:33:00',
+        imgUrls: [],
+        title: 'K458 衡山站 - 长沙站',
+        content: '从衡山站出发，前往长沙'
+    }, {
+        timestamp: '2020-01-19 11:19:00',
+        imgUrls: [],
+        title: 'K458 衡山站 - 长沙站',
+        content: '到达长沙站'
+    }, {
+        timestamp: '2020-01-18 10:00:00',
+        imgUrls: [],
+        title: '确认火车有没有晚点',
+        content: '查看时间，确认火车有没有晚点<br>' +
+            '如果可能晚点的话尽快改签下一趟火车<br>' +
+            '或者搜寻公车路线'
+    }, {
+        timestamp: '2020-01-20 07:45:00',
+        imgUrls: [],
+        title: 'K968 长沙站 - 岳阳站',
+        content: '从长沙站出发，前往岳阳'
+    }, {
+        timestamp: '2020-01-20 09:13:00',
+        imgUrls: [],
+        title: 'K968 长沙站 - 岳阳站',
+        content: '到达岳阳站'
+    }, {
+        timestamp: '2020-01-20 15:40:00',
+        imgUrls: [],
+        title: '中国联合航空KN5806 岳阳三荷机场 - 北京大兴国际机场',
+        content: '从岳阳三荷机场出发，前往北京'
+    }, {
+        timestamp: '2020-01-20 18:00:00',
+        imgUrls: [],
+        title: '中国联合航空KN5806 岳阳三荷机场 - 北京大兴国际机场',
+        content: '到达北京大兴国际机场'
+    }, {
+        timestamp: '2020-01-17 22:00:00',
+        imgUrls: [],
+        title: '金地花园 - 深圳站',
+        content: '从金地花园出发，前往深圳站<br>' +
+            '7号线（太安方向）上沙站 - 石厦站<br>' +
+            '3号线（双龙方向）石厦站 - 老街站<br>' +
+            '1号线（罗湖方向）老街站 - 罗湖站',
+        amapUrl: 'https://surl.amap.com/1jfRJGk1x2Qm'
+    },
+
+
+    {
+        timestamp: '2019/10/03 07:00:00',
+        imgUrls: [
+            '../images/ylxs-mp.png',
+            '../images/ylxs-mp-01.png',
+            '../images/ylxs-mp-02.png'
+        ],
+        title: '<strong style="color: orangered;">抢票</strong> 玉龙雪山景点门票',
+        members: '李、浮、谭、党',
+        content: '提前抢票<br>' +
+            '官方订票(只能订当日)：丽江旅游集团公众号(7:00 - 21:00)<br>' +
+            '进山费：景区大门口购买，¥100/人'
+    }, {
+        timestamp: '2019/10/03 11:40:00',
+        imgUrls: [
+            '../images/ht-ylxs.png',
+            '../images/ht-ylxs-02.png',
+            '../images/ylxsjqzx.png'
+        ],
+        title: '打车 汉庭丽江古城福星路酒店 - 玉龙雪山游客中心',
+        members: '李、浮、谭、党',
+        content: '从汉庭丽江古城福星路酒店出发，前往玉龙雪山。<br>' +
+            '打车：大约 ¥80～100，40~50 分钟。<br>' +
+            '公交：大约 ¥15/人，1 小时 31 分钟。<br>' +
+            '建议坐<a href="http://www.sohu.com/a/307119287_123580">旅游专线的公交</a>，在车上就可以买进山的大门票。',
+    }, {
+        timestamp: '2019/10/03 19:20:00',
+        imgUrls: [
+            '../images/ylxs-ht-01.png',
+            '../images/ylxs-ht-02.png'
+        ],
+        title: '打车 景区门口 - 汉庭丽江古城福星路酒店',
+        members: '李、浮、谭、党',
+        content: '从景区门口出发，前往汉庭丽江古城福星路酒店。<br>' +
+            '打车：大约 ¥80 ～ 100，40 ～ 60 分钟<br>' +
+            '黑车：大约 ¥20 ～ 40/人，40 ～ 60 分钟<br>' +
+            '回程路上可以<br><a href="https://surl.amap.com/1Obea9Nf7ZN" style="color: orangered;">1. 提前预定</a>第二天早上从酒店到火车站的车<br>' +
+            '2. 挑选回古城后吃饭的地方 <a href="http://www.dianping.com/lijiang/ch10/r8330o3" style="color: orangered;">大众点评</a>',
+        amapUrl: 'https://surl.amap.com/1Qqh3tt1lfjq'
+    }];

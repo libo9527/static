28c3eeb716025d88235e17b6dae492eebe8fe838
bli <bli@skystartrade.com>
2020-01-15 14:10:18 +0800

    update

--- map/amap/2020-0117-0120/timeline/data.js
+++ map/amap/2020-0117-0120/timeline/data.js
@@ -16,156 +16,73 @@ const data = [{
     title: 'K9001 株洲站 - 衡山站',
     members: '李',
     content: '到达衡山站'
-}, {
-    timestamp: '2020-01-18 10:59:00',
-    imgUrls: [],
-    title: 'K9001 株洲站 - 衡山站',
-    members: '李',
-    content: '从株洲站出发，前往衡山'
-}, {
-    timestamp: '2020-01-20 07:45:00',
-    imgUrls: [],
-    title: 'K968 长沙站 - 岳阳站',
-    members: '李',
-    content: '从长沙站出发，前往岳阳'
-}, {
-    timestamp: '2020-01-20 09:13:00',
-    imgUrls: [],
-    title: 'K968 长沙站 - 岳阳站',
-    members: '李',
-    content: '到达岳阳站'
-}, {
-    timestamp: '2020-01-20 15:40:00',
-    imgUrls: [],
-    title: '中国联合航空KN5806 岳阳三荷机场 - 北京大兴国际机场',
-    members: '李',
-    content: '从岳阳三荷机场出发，前往北京'
-}, {
-    timestamp: '2020-01-20 18:00:00',
-    imgUrls: [],
-    title: '中国联合航空KN5806 岳阳三荷机场 - 北京大兴国际机场',
-    members: '李',
-    content: '到达北京大兴国际机场'
-}, {
-    timestamp: '2020-01-17 22:00:00',
-    imgUrls: [],
-    title: '金地花园 - 深圳站',
-    members: '李',
-    content: '从金地花园出发，前往深圳站<br>' +
-        '7号线（太安方向）上沙站 - 石厦站<br>' +
-        '3号线（双龙方向）石厦站 - 老街站<br>' +
-        '1号线（罗湖方向）老街站 - 罗湖站',
-    amapUrl: 'https://surl.amap.com/1jfRJGk1x2Qm'
 },
 
 
     {
-        timestamp: '2019/10/02 12:56:00',
-        imgUrls: [
-            '../images/gzn-kmn.png'
-        ],
-        title: '动车 广州南 - 昆明南',
-        members: '李、浮',
-        content: '从广州东出发，前往昆明'
-    }, {
-        timestamp: '2019/10/02 21:01:00',
+        timestamp: '2020-01-18 10:59:00',
         imgUrls: [],
-        title: '动车 广州南 - 昆明南',
-        members: '李、浮',
-        content: '到达昆明南站'
+        title: 'K9001 株洲站 - 衡山站',
+        members: '李',
+        content: '从株洲站出发，前往衡山'
     }, {
-        timestamp: '2019/10/02 21:30:00',
-        imgUrls: [
-            '../images/kmn-ht-01.gif'
-        ],
-        title: '地铁 昆明南 - 汉庭优佳昆明东华酒店',
-        members: '李、浮',
-        content: '从昆明南站出发，前往酒店。大约 1 小时 20 分钟。',
-        amapUrl: 'https://surl.amap.com/11oyN5l8z5'
-    }, {
-        timestamp: '2019/10/02 22:50:00',
+        timestamp: '2020-01-19 09:33:00',
         imgUrls: [],
-        title: '地铁 昆明南 - 汉庭优佳昆明东华酒店',
-        members: '李、浮',
-        content: '到达汉庭优佳昆明东华酒店',
+        title: 'K458 衡山站 - 长沙站',
+        members: '李',
+        content: '从衡山站出发，前往长沙'
     }, {
-        timestamp: '2019/10/03 00:30:00',
-        imgUrls: [
-            '../images/jc-ht-01.gif'
-        ],
-        title: '机场大巴 昆明长水国际机场 - 汉庭优佳昆明东华酒店',
-        members: '谭、党',
-        content: '从昆明长水国际机场出发，前往酒店。大约 50 分钟。',
-        amapUrl: 'https://surl.amap.com/12BIkRFr7FK'
-    }, {
-        timestamp: '2019/10/03 01:20:00',
+        timestamp: '2020-01-19 11:19:00',
         imgUrls: [],
-        title: '机场大巴 昆明长水国际机场 - 汉庭优佳昆明东华酒店',
-        members: '谭、党',
-        content: '到达汉庭优佳昆明东华酒店',
+        title: 'K458 衡山站 - 长沙站',
+        members: '李',
+        content: '到达长沙站'
     }, {
-        timestamp: '2019/10/03 06:30:00',
-        imgUrls: [
-            '../images/ht-km.png'
-        ],
-        title: '打车 汉庭优佳昆明东华酒店 - 昆明站',
-        members: '李、浮、谭、党',
-        content: '从汉庭优佳昆明东华酒店出发，前往昆明站。大约 20 分钟。',
-        amapUrl: 'https://surl.amap.com/xzMplVqeCQ'
-    }, {
-        timestamp: '2019/10/03 07:00:00',
+        timestamp: '2020-01-18 10:00:00',
         imgUrls: [],
-        title: '打车 汉庭优佳昆明东华酒店 - 昆明站',
-        members: '李、浮、谭、党',
-        content: '到达昆明站',
+        title: '确认火车有没有晚点',
+        members: '李',
+        content: '查看时间，确认火车有没有晚点<br>' +
+            '如果可能晚点的话尽快改签下一趟火车<br>' +
+            '或者搜寻公车路线'
     }, {
-        timestamp: '2019/10/03 07:14:00',
-        imgUrls: [
-            '../images/km-lj.png'
-        ],
-        title: '动车 昆明站 - 丽江站',
-        members: '李、浮、谭、党',
-        content: '从昆明站出发，前往丽江'
-    }, {
-        timestamp: '2019/10/03 10:43:00',
+        timestamp: '2020-01-20 07:45:00',
         imgUrls: [],
-        title: '动车 昆明站 - 丽江站',
-        members: '李、浮、谭、党',
-        content: '到达丽江'
+        title: 'K968 长沙站 - 岳阳站',
+        members: '李',
+        content: '从长沙站出发，前往岳阳'
     }, {
-        timestamp: '2019/10/03 11:00:00',
-        imgUrls: [
-            '../images/lj-ht.png'
-        ],
-        title: '打车 丽江站 - 汉庭丽江古城福星路酒店',
-        members: '李、浮、谭、党',
-        content: '从丽江站出发，前往汉庭丽江古城福星路酒店。大约 ¥30～40，30～40分钟。',
-        amapUrl: 'https://surl.amap.com/AkKvo71n2Ru'
+        timestamp: '2020-01-20 09:13:00',
+        imgUrls: [],
+        title: 'K968 长沙站 - 岳阳站',
+        members: '李',
+        content: '到达岳阳站'
     }, {
-        timestamp: '2019/10/01 07:00:00',
-        imgUrls: [
-            '../images/ylxs-mp.png',
-            '../images/ylxs-mp-01.png',
-            '../images/ylxs-mp-02.png'
-        ],
-        title: '看票 玉龙雪山景点门票',
-        members: '李、浮、谭、党',
-        content: '提前试一下看能不能在官方网站抢到票，如果比较难强就要考虑在其他三方上购票了<br>' +
-            '官方订票(只能订当日)：丽江旅游集团公众号(7:00 - 21:00)<br>' +
-            '进山费：景区大门口购买，¥100/人'
+        timestamp: '2020-01-20 15:40:00',
+        imgUrls: [],
+        title: '中国联合航空KN5806 岳阳三荷机场 - 北京大兴国际机场',
+        members: '李',
+        content: '从岳阳三荷机场出发，前往北京'
     }, {
-        timestamp: '2019/10/02 07:00:00',
-        imgUrls: [
-            '../images/ylxs-mp.png',
-            '../images/ylxs-mp-01.png',
-            '../images/ylxs-mp-02.png'
-        ],
-        title: '看票 玉龙雪山景点门票',
-        members: '李、浮、谭、党',
-        content: '提前试一下看能不能在官方网站抢到票，如果比较难强就要考虑在其他三方上购票了<br>' +
-            '官方订票(只能订当日)：丽江旅游集团公众号(7:00 - 21:00)<br>' +
-            '进山费：景区大门口购买，¥100/人'
+        timestamp: '2020-01-20 18:00:00',
+        imgUrls: [],
+        title: '中国联合航空KN5806 岳阳三荷机场 - 北京大兴国际机场',
+        members: '李',
+        content: '到达北京大兴国际机场'
     }, {
+        timestamp: '2020-01-17 22:00:00',
+        imgUrls: [],
+        title: '金地花园 - 深圳站',
+        members: '李',
+        content: '从金地花园出发，前往深圳站<br>' +
+            '7号线（太安方向）上沙站 - 石厦站<br>' +
+            '3号线（双龙方向）石厦站 - 老街站<br>' +
+            '1号线（罗湖方向）老街站 - 罗湖站',
+        amapUrl: 'https://surl.amap.com/1jfRJGk1x2Qm'
+    },
+
+
+    {
         timestamp: '2019/10/03 07:00:00',
         imgUrls: [
             '../images/ylxs-mp.png',
@@ -177,30 +94,6 @@ const data = [{
         content: '提前抢票<br>' +
             '官方订票(只能订当日)：丽江旅游集团公众号(7:00 - 21:00)<br>' +
             '进山费：景区大门口购买，¥100/人'
-    }, {
-        timestamp: '2019/10/03 11:30:00',
-        imgUrls: [
-            '../images/ylxsjqzx.png',
-            '../images/ht-ylxs.png',
-            '../images/ht-ylxs-02.png'
-        ],
-        title: '购物 忠义市场购买氧气罐',
-        members: '谭、党',
-        content: '忠义市场购物清单：<br>' +
-            '1. 氧气罐：¥20～30/罐，建议买三罐<br>' +
-            '2. 巧克力/士力架<br>' +
-            '购买完后从忠义市场站乘车，在红太阳广场站汇合。'
-    }, {
-        timestamp: '2019/10/03 11:40:00',
-        imgUrls: [
-            '../images/ylxsjqzx.png',
-            '../images/ht-ylxs.png',
-            '../images/ht-ylxs-02.png'
-        ],
-        title: '打车 丽江站 - 汉庭丽江古城福星路酒店',
-        members: '李、浮',
-        content: '到达汉庭丽江古城福星路酒店，办理入住，寄存行李。<br>' +
-            '之后前往红太阳广场站乘车汇合。'
     }, {
         timestamp: '2019/10/03 11:40:00',
         imgUrls: [
@@ -215,44 +108,6 @@ const data = [{
             '公交：大约 ¥15/人，1 小时 31 分钟。<br>' +
             '建议坐<a href="http://www.sohu.com/a/307119287_123580">旅游专线的公交</a>，在车上就可以买进山的大门票。',
     }, {
-        timestamp: '2019/10/03 13:00:00',
-        imgUrls: [],
-        title: '打车 汉庭丽江古城福星路酒店 - 玉龙雪山游客中心',
-        members: '李、浮、谭、党',
-        content: '12:30 ~ 13:00 到达玉龙雪山游客中心'
-    }, {
-        timestamp: '2019/10/03 13:00:00',
-        imgUrls: [],
-        title: '坐车 玉龙雪山游客中心 - 蓝月谷',
-        members: '李、浮、谭、党',
-        content: '从玉龙雪山游客中心出发，前往蓝月谷。大约 20～30 分钟。'
-    }, {
-        timestamp: '2019/10/03 13:30:00',
-        imgUrls: [],
-        title: '坐车 玉龙雪山游客中心 - 蓝月谷',
-        members: '李、浮、谭、党',
-        content: '到达蓝月谷。<br>' +
-            '游览时间为 60 分钟。早走早去大索道排队，否则赶不上大索道就惨了。'
-    }, {
-        timestamp: '2019/10/03 14:30:00',
-        imgUrls: [],
-        title: '坐车 蓝月谷 - 大索道',
-        members: '李、浮、谭、党',
-        content: '从蓝月谷出发，前往大索道<br>' +
-            '排队大约需要 2 ～ 4 个小时，索道最晚一趟是 17:30 ～ 18:00'
-    }, {
-        timestamp: '2019/10/03 19:00:00',
-        imgUrls: [],
-        title: '坐车 大索道 - 景区门口',
-        members: '李、浮、谭、党',
-        content: '从大索道出发，前往景区门口'
-    }, {
-        timestamp: '2019/10/03 19:20:00',
-        imgUrls: [],
-        title: '坐车 大索道 - 景区门口',
-        members: '李、浮、谭、党',
-        content: '到达景区门口。路上提前打回酒店的车！'
-    }, {
         timestamp: '2019/10/03 19:20:00',
         imgUrls: [
             '../images/ylxs-ht-01.png',
@@ -266,68 +121,4 @@ const data = [{
             '回程路上可以<br><a href="https://surl.amap.com/1Obea9Nf7ZN" style="color: orangered;">1. 提前预定</a>第二天早上从酒店到火车站的车<br>' +
             '2. 挑选回古城后吃饭的地方 <a href="http://www.dianping.com/lijiang/ch10/r8330o3" style="color: orangered;">大众点评</a>',
         amapUrl: 'https://surl.amap.com/1Qqh3tt1lfjq'
-    }, {
-        timestamp: '2019/10/03 20:20:00',
-        imgUrls: [],
-        title: '打车 景区门口 - 汉庭丽江古城福星路酒店',
-        members: '李、浮、谭、党',
-        content: '到达汉庭丽江古城福星路酒店。'
-    }, {
-        timestamp: '2019/10/04 06:30:00',
-        imgUrls: [
-            '../images/ht-lj-01.png',
-            '../images/ht-lj-02.png'
-        ],
-        title: '打车 汉庭丽江古城福星路酒店 - 丽江站',
-        members: '李、浮、谭、党',
-        content: '从汉庭丽江古城福星路酒店出发，前往丽江站。大约 ¥35，30 分钟',
-        amapUrl: 'https://surl.amap.com/1Obea9Nf7ZN'
-    }, {
-        timestamp: '2019/10/04 07:00:00',
-        imgUrls: [],
-        title: '打车 汉庭丽江古城福星路酒店 - 丽江站',
-        members: '李、浮、谭、党',
-        content: '到达丽江站'
-    }, {
-        timestamp: '2019/10/04 07:38:00',
-        imgUrls: [
-            '../images/lj-dl.png'
-        ],
-        title: '快车 丽江站 - 大理站',
-        members: '李、浮、谭、党',
-        content: '从丽江站出发，前往大理'
-    }, {
-        timestamp: '2019/10/04 09:30:00',
-        imgUrls: [],
-        title: '快车 丽江站 - 大理站',
-        members: '李、浮、谭、党',
-        content: '到达大理'
-    }, {
-        timestamp: '2019/10/05 20:50:00',
-        imgUrls: [
-            '../images/dl-km.png'
-        ],
-        title: '动车 大理站 - 昆明站',
-        members: '李、浮、谭、党',
-        content: '从大理站出发，前往昆明'
-    }, {
-        timestamp: '2019/10/05 22:45:00',
-        imgUrls: [],
-        title: '动车 大理站 - 昆明站',
-        members: '李、浮、谭、党',
-        content: '到达昆明站'
-    }, {
-        timestamp: '2019/10/07 07:40:00',
-        imgUrls: [
-            '../images/kmn-gzn.png'
-        ],
-        title: '动车 昆明南 - 广州南',
-        members: '李、浮',
-        content: '从昆明南站出发，前往广州'
-    }, {
-        timestamp: '2019/10/07 15:27:00',
-        imgUrls: [],
-        title: '动车 昆明南 - 广州南',
-        members: '李、浮',
-        content: '到达广州南站'
     }];

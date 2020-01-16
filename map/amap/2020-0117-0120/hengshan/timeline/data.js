95338efdf59dddc4fa8f14e36bbf38fb551cc407
bli <bli@skystartrade.com>
2020-01-16 10:01:11 +0800

    update

--- map/amap/2020-0117-0120/hengshan/timeline/data.js
+++ map/amap/2020-0117-0120/hengshan/timeline/data.js
@@ -34,46 +34,56 @@ const data = [{
 }, {
     timestamp: '2020-01-18 13:50:00',
     imgUrls: [],
-    title: '华严湖 - 忠烈祠',
-    content: '从华严湖出发，前往忠烈祠<br>' +
-        '<span style="color: orange">走路大约需要 21 分钟</span>',
-    amapUrl: 'https://surl.amap.com/1wMObEkk5Ys'
+    title: '华严湖 - 中桎木潭',
+    content: '从华严湖出发，前往中桎木潭<br>' +
+        '<span style="color: orange">走路大约需要 19 分钟</span>'
 }, {
     timestamp: '2020-01-18 14:10:00',
     imgUrls: [],
-    title: '华严湖 - 忠烈祠',
-    content: '到达忠烈祠<br>' +
-        '<span style="color: orangered">逗留 15～20 分钟</span>',
-    amapUrl: 'https://surl.amap.com/1wMObEkk5Ys'
+    title: '华严湖 - 中桎木潭',
+    content: '到达中桎木潭<br>' +
+        '<span style="color: orangered">逗留 3～5 分钟</span>'
 }, {
-    timestamp: '2020-01-18 14:30:00',
+    timestamp: '2020-01-18 14:15:00',
+    imgUrls: [],
+    title: '中桎木潭 - 忠烈祠',
+    content: '从中桎木潭出发，前往忠烈祠<br>' +
+        '<span style="color: orange">走路大约需要 9 分钟</span>'
+}, {
+    timestamp: '2020-01-18 14:25:00',
+    imgUrls: [],
+    title: '中桎木潭 - 忠烈祠',
+    content: '到达忠烈祠<br>' +
+        '<span style="color: orangered">逗留 10～15 分钟</span>'
+}, {
+    timestamp: '2020-01-18 14:40:00',
     imgUrls: [],
     title: '忠烈祠 - 梵音谷',
     content: '从忠烈祠出发，前往梵音谷<br>' +
         '<span style="color: orange">走路大约需要 5 分钟</span>',
     amapUrl: 'https://surl.amap.com/1wMObEkk5Ys'
 }, {
-    timestamp: '2020-01-18 14:35:00',
+    timestamp: '2020-01-18 14:45:00',
     imgUrls: [
         'http://n1-q.mafengwo.net/s10/M00/BA/60/wKgBZ1k4DdCAV3erABAgalA5Q3Y22.jpeg?imageView2%2F2%2Fw%2F1360%2Fq%2F90'
     ],
     title: '忠烈祠 - 梵音谷',
     content: '到达梵音谷<br>' +
-        '<span style="color: orangered">逗留 10～15 分钟</span>',
+        '<span style="color: orangered">逗留 5～10 分钟</span>',
     amapUrl: 'https://surl.amap.com/1wMObEkk5Ys'
 }, {
-    timestamp: '2020-01-18 14:50:00',
+    timestamp: '2020-01-18 14:55:00',
     imgUrls: [],
     title: '梵音谷 - 穿岩诗林',
     content: '从梵音谷出发，前往穿岩诗林<br>' +
         '<span style="color: orange">走路大约需要 15 分钟</span>',
     amapUrl: 'https://surl.amap.com/1AxVnsQ128v'
 }, {
-    timestamp: '2020-01-18 15:05:00',
+    timestamp: '2020-01-18 15:10:00',
     imgUrls: [],
     title: '梵音谷 - 穿岩诗林',
     content: '到达穿岩诗林<br>' +
-        '<span style="color: orangered">逗留 5～10 分钟</span>',
+        '<span style="color: orangered">逗留 3～5 分钟</span>',
     amapUrl: 'https://surl.amap.com/1wMObEkk5Ys'
 }, {
     timestamp: '2020-01-18 15:15:00',
@@ -159,45 +169,4 @@ const data = [{
     content: '到达祝融峰<br>' +
         '<span style="color: orangered">逗留 10～20 分钟</span>',
     amapUrl: 'https://surl.amap.com/f0xe8432TF'
-},
-
-    {
-        timestamp: '2019/10/03 07:00:00',
-        imgUrls: [
-            '../images/ylxs-mp.png',
-            '../images/ylxs-mp-01.png',
-            '../images/ylxs-mp-02.png'
-        ],
-        title: '<strong style="color: orangered;">抢票</strong> 玉龙雪山景点门票',
-        members: '李、浮、谭、党',
-        content: '提前抢票<br>' +
-            '官方订票(只能订当日)：丽江旅游集团公众号(7:00 - 21:00)<br>' +
-            '进山费：景区大门口购买，¥100/人'
-    }, {
-        timestamp: '2019/10/03 11:40:00',
-        imgUrls: [
-            '../images/ht-ylxs.png',
-            '../images/ht-ylxs-02.png',
-            '../images/ylxsjqzx.png'
-        ],
-        title: '打车 汉庭丽江古城福星路酒店 - 玉龙雪山游客中心',
-        members: '李、浮、谭、党',
-        content: '从汉庭丽江古城福星路酒店出发，前往玉龙雪山。<br>' +
-            '打车：大约 ¥80～100，40~50 分钟。<br>' +
-            '公交：大约 ¥15/人，1 小时 31 分钟。<br>' +
-            '建议坐<a href="http://www.sohu.com/a/307119287_123580">旅游专线的公交</a>，在车上就可以买进山的大门票。',
-    }, {
-        timestamp: '2019/10/03 19:20:00',
-        imgUrls: [
-            '../images/ylxs-ht-01.png',
-            '../images/ylxs-ht-02.png'
-        ],
-        title: '打车 景区门口 - 汉庭丽江古城福星路酒店',
-        members: '李、浮、谭、党',
-        content: '从景区门口出发，前往汉庭丽江古城福星路酒店。<br>' +
-            '打车：大约 ¥80 ～ 100，40 ～ 60 分钟<br>' +
-            '黑车：大约 ¥20 ～ 40/人，40 ～ 60 分钟<br>' +
-            '回程路上可以<br><a href="https://surl.amap.com/1Obea9Nf7ZN" style="color: orangered;">1. 提前预定</a>第二天早上从酒店到火车站的车<br>' +
-            '2. 挑选回古城后吃饭的地方 <a href="http://www.dianping.com/lijiang/ch10/r8330o3" style="color: orangered;">大众点评</a>',
-        amapUrl: 'https://surl.amap.com/1Qqh3tt1lfjq'
-    }];
+}];
